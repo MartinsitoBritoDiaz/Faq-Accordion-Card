fb1c6338e9fffbb875bbb7bfb808a74c85e74ddc
Martinsito <martinsitobd@gmail.com>
2022-10-19 09:30:06 -0400

    Agrenando el saludo

---
+++ main.js
@@ -0,0 +1,9 @@
+
+const user = {
+    name: 'Martinsito',
+    email: 'martinsitobd@gmail.com',
+    password: 'sduih32erdsdnb',
+    avatar: 'https://picture.com/3',
+}
+const saludo = 'hello';
+console.table(user);
